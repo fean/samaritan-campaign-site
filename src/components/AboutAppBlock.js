4ea27d07bf53d918e7c5f79f7ea45bc1d7a01d07
Leonard Breitkopf <leonard@trunkrs.nl>
2022-03-15 00:27:04 +0100

    Fix centering

--- src/components/AboutAppBlock.js
+++ src/components/AboutAppBlock.js
@@ -1,11 +1,11 @@
 import * as React from "react"
 
 const AboutAppBlock = () => (
-  <div className="container overflow-hidden flex flex-col mx-auto px-3 pt-16 pb-16 md:pt-20 md:pb-20">
+  <div className="container overflow-hidden max-w-4xl flex flex-col mx-auto px-3 pt-16 pb-16 md:pt-20 md:pb-20">
     <h1 className="font-bold pb-10 text-center text-[30px] md:text-[32px] text-[#001133]">
       Our mission
     </h1>
-    <p className="font-black font-normal text-center max-w-4xl text-[18px] text-[#3C455D]">
+    <p className="font-black font-normal text-center text-[18px] text-[#3C455D]">
       The humanitarian refugee effort has been miraculous in the last couple of
       weeks. So many amazing foundations and groups of people have put their
       foot forward and are helping refugees where they can.
